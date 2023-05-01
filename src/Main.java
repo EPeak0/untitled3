acc3ab5bd941fa596ccf4943d2b103235417c337
theot <theo.tissieres@students.hevs.ch>
2023-05-01 15:43:21 +0200

    TestTheo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     Person prs = new Person("testName", 45);
     public static void main(String[] args) {
-        System.out.println("j'espere ca marche sinon je casse tout");
+        System.out.println("TEST");
     }
 }
