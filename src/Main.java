f5c1d3eb79c557559ee70e359fae7bed7e73a44a
theot <theo.tissieres@students.hevs.ch>
2023-05-01 15:04:36 +0200

    TestTheo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world 3!");
+        System.out.println("j'espere ca marche sinon je casse tout");
     }
 }
